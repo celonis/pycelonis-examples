96c98d6ab668184d9c5d62edec2012714c511942
owenwu811 <o.wu@celonis.com>
2024-11-21 13:55:15 -0500

    Update files in datamoverproject

--- csvtoparquet.py
+++ csvtoparquet.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # Load the CSV file
-csv_file_path = "/Users/o.wu/Downloads/air_quality_no2.csv"
+csv_file_path = ""
 data = pd.read_csv(csv_file_path)
 
 # Convert to Parquet
-parquet_file_path = "/Users/o.wu/Downloads/newairqualityparquet.parquet"
+parquet_file_path = ""
 data.to_parquet(parquet_file_path, index=False)
 
 print("Conversion to Parquet completed.")
